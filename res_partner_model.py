c0a606bd569566ca8b5283099149bae1d17ac577
fabiomix <fabiomix@users.noreply.github.com>
2017-08-23 15:35:17 +0200

    name_search fix

--- res_partner_model.py
+++ res_partner_model.py
@@ -9,25 +9,13 @@ class partner(models.Model):
 
 
     @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=100):
-        args = args or []
-        res = []
-        if name:
-            recs = self.search([('internal_code', operator, name)] + args, limit=limit, context=context)
-            res = self.name_get(recs)
-        res += super(partner, self).name_search(name, args, operator=operator, limit=limit)
-        return res
-
-
-    @api.model
     def create(self, vals):
         if not vals.get('internal_code', False):
-            vals['internal_code'] = self.env[
-                'ir.sequence'].get('partner.internal.code') or '/'
-                # 'ir.sequence'].next_by_code('partner.internal.code') or '/'
+            # vals['internal_code'] = self.env['ir.sequence'].get('partner.internal.code') or '/'
+            vals['internal_code'] = self.env['ir.sequence'].next_by_code('partner.internal.code') or '/'
         return super(partner, self).create(vals)
 
 
     _sql_constraints = {
-        ('internal_code_uniq', 'unique(internal_code)', 'Internal Code mast be unique!')
+        ('internal_code_uniq', 'unique(internal_code)', 'Internal Code must be unique!')
     }
