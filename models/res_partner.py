26cb3848574ad1d600b217f3e398ad94bd98f446
fabiomix <fabiomix@users.noreply.github.com>
2018-05-21 12:37:26 +0200

    dont copy code on partner duplication

--- models/res_partner.py
+++ models/res_partner.py
@@ -5,24 +5,23 @@ from odoo import models, fields, api
 class partner(models.Model):
     _inherit = 'res.partner'
 
-    internal_code = fields.Char('Internal Code', index=True)
+    _sql_constraints = {
+        ('internal_code_uniq', 'unique(internal_code)', 'Internal Code must be unique!')
+    }
+
+    # new fields
+    internal_code = fields.Char('Internal Code', index=True, copy=False)
 
 
     @api.model
     def create(self, vals):
-        if not vals.get('internal_code', False):
-            # vals['internal_code'] = self.env['ir.sequence'].get('partner.internal.code') or '/'
-            # vals['internal_code'] = self.env['ir.sequence'].next_by_code('partner.internal.code') or '/'
 
+        if not vals.get('internal_code', False):
             sq = self.env['ir.sequence'].next_by_code('partner.internal.code')
+
             while self.env['res.partner'].search_count([('internal_code', '=', sq)]):
-                ## code already used, request another one
+                # code already used, request another one
                 sq = self.env['ir.sequence'].next_by_code('partner.internal.code')
 
             vals['internal_code'] = sq
         return super(partner, self).create(vals)
-
-
-    _sql_constraints = {
-        ('internal_code_uniq', 'unique(internal_code)', 'Internal Code must be unique!')
-    }
